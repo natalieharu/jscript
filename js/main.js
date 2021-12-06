00fb93a2188ebd136fbf94b7a9a02366edef4de0
Natalie <natalicooper@gmail.com>
2021-12-06 19:12:19 +0200

    Start

---
+++ js/main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// alert('helo');
+
+// const result = confirm('Are you here?');
+// console.log(result);
+// const answer = prompt("Вам есть 18 лет", "получим строку"); 
+// const answer = +prompt("Вам есть 18 лет", "получим число"); 
+// console.log(answer);
+
+// const answers = [];
+
+// answers[0] = prompt("Как вас зовут?", "");
+// answers[1] = prompt("Какая ваша фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+// console.log(typeof(answers)); typeof - проверяет какой тип данных
+
+// const natalie = "natalie.cooper.332";
+// console.log(`https://www.facebook.com/${natalie}`);
+
+// const user = "Natali";
+// console.log(`Hello, ${user}`); - интерполяция
+
+let  incr = 10;
+let  decr = 10;
+
+// Инкримент - увиличение на единицу
+// incr++;      постфиксная запись
+// ++incr;      префиксная запись
+
+// Декримент - уменшение на единицу
+// decr--;
+
+// ==     - равно по значению
+// ===    - равно по типу данных
+
+//     &&    - оператор "И"
+//     ||    - оператор "ИЛИ"
+//    "!"    - знак отрецания
